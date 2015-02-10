1decf903cec2dc6b1b875bae8a848296e939fab5
Peter Furey <prfurey@gmail.com>
2015-02-10 15:11:33 -0700

    Add CRUD capabilities to Tasks page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,20 +8,39 @@ class TasksController < ApplicationController
       @task = Task.new
     end
 
-    # def show
-    #   @task = Task.find(params[:id])
-    # end
+    def create
+      task = Task.new(task_params)
+      task.save
+      redirect_to task_path(task)
+    end
 
-    # def edit
-    # end
-    #
-    # def destroy
-    # end
+    def show
+      @task = Task.find(params[:id])
+    end
+
+    def edit
+      @task = Task.find(params[:id])
+    end
+
+    def update
+      task = Task.find(params[:id])
+      if task.update(task_params)
+        redirect_to task_path(task)
+      else
+        render :edit   # show edit form again; ie. complete action be show other view
+      end
+    end
+
+    def destroy
+      @task = Task.find(params[:id])
+      @task.destroy
+      redirect_to tasks_path
+    end
 
     private
 
     def task_params
-      params.require(:task).permit(:title)
+      params.require(:task).permit(:description)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,12 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # Below equivalent to get '/' => 'welcome#index'
-  # Add root controller
     root 'welcome#index'
 
   # Add other controllers
     get 'terms' => 'terms#view'
     get 'about' => 'about#view'
     get 'faq' => 'common_questions#view'
-    resources :tasks, only: [:index, :new, :show]
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :tasks
+    # , only: [:index, :new, :create, :show, :edit, :update]
 
 end
