00539f9c0e33c684ea259e5a2f7ef972ba93f343
Peter Furey <prfurey@gmail.com>
2015-02-19 14:36:49 -0700

    Moved due date in show page of edits and added delete confirm message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,13 +36,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user=User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
 
   private
 
   def user_params
-
-    params.require(:user).permit(:first_name, :last_name, :email)
-
+    params.require(:user).permit(:first_name,:last_name,:email)
   end
 
 end
