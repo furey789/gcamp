e8d45705cd6ffaa004f9dd0c8ecbd7a4b6f7236d
Peter Furey <prfurey@gmail.com>
2015-02-19 06:35:30 -0700

    Add Edit button to Users index page and Add Edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,23 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user=User.find(params[:id])
   end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="User was successfully updated"
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
 
   private
 
