5d81e3475d948131464cb8346f6368f7bc239ebc
Peter Furey <prfurey@gmail.com>
2015-02-22 16:21:07 -0700

    Add Projects Index and New pages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,27 @@
 class ProjectsController < ApplicationController
 
   def index
-
+    @projects=Project.all
   end
-  
+
+  def new
+    @project=Project.new
+  end
+
+  def create
+    @project=Project.new(project_params)
+    @project.save
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218213510) do
+ActiveRecord::Schema.define(version: 20150220232400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.text     "description"
