d2f3c071135f996569a73f42ebe3d0693b94f010
Peter Furey <prfurey@gmail.com>
2015-02-23 13:13:36 -0700

    Add functioning Edit button with update message on show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,6 +23,20 @@ class ProjectsController < ApplicationController
     @project=Project.find(params[:id])
   end
 
+  def edit
+    @project=Project.find(params[:id])
+  end
+
+  def update
+    @project=Project.find(params[:id])
+    if @project.update(project_params)
+      flash[:notice]="Project was successfully updated"
+      redirect_to project_path(@project)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @project=Project.find(params[:id])
     @project.destroy
