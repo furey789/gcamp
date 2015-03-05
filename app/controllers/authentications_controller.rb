1ad2fb39a285eabfc63b9b7644af6a8b15c84a7b
Peter Furey <prfurey@gmail.com>
2015-03-05 06:47:09 -0700

    Add flash notice for Sign In

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,6 +3,7 @@ class AuthenticationsController < ApplicationController
 
   def destroy
     session[:user_id]=nil
+    flash[:notice]="You have successfully logged out"
     redirect_to root_path
   end
 
@@ -13,6 +14,7 @@ class AuthenticationsController < ApplicationController
     @user=User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id]=@user.id
+      flash[:notice]="You have successfully signed in"
       redirect_to root_path
     else
       render :new
