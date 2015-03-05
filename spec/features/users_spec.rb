3b3811c495d409e56fb1e255732684dd06bab14e
Peter Furey <prfurey@gmail.com>
2015-03-04 21:02:50 -0700

    Modify rspec tests of features

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -10,9 +10,14 @@ feature "Users" do
       password:"js", password_confirmation: "js")
   end
 
-  scenario "user can create another user" do
+  scenario "user can create, read, and delete another user" do
 
     visit root_path
+    click_link 'Sign In'
+    fill_in "Email", with: "js@gmail.com"
+    fill_in "Password", with: 'js'
+    click_button 'Sign In'
+
     click_link 'Users'
     expect(current_path).to eq '/users'
 
@@ -29,11 +34,27 @@ feature "Users" do
     expect(page).to have_content 'User was successfully created'
     expect(page).to have_content 'Users'
 
+    click_link 'Mo Nee'
+    expect(page).to have_content 'Mo Nee'
+    expect(page).to have_content 'mn@ex.com'
+
+    click_link 'Edit'
+    expect(page).to have_content 'Delete User'
+
+    click_link 'Delete User'
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'User was successfully deleted'
+
   end
 
   scenario "user can edit another user" do
 
     visit root_path
+    click_link 'Sign In'
+    fill_in "Email", with: "js@gmail.com"
+    fill_in "Password", with: 'js'
+    click_button 'Sign In'
+
     click_link 'Users'
     expect(current_path).to eq '/users'
 
@@ -52,36 +73,14 @@ feature "Users" do
 
   end
 
-  scenario "user can delete another user" do
-
-    visit root_path
-    click_link 'Users'
-    expect(current_path).to eq '/users'
-
-    click_link 'Edit'
-    expect(page).to have_content 'Delete User'
-
-    click_link 'Delete User'
-    expect(current_path).to eq '/users'
-    expect(page).to have_content 'User was successfully deleted'
-
-  end
-
-  scenario "user can read about a user on show page" do
-
-    visit root_path
-    click_link 'Users'
-    expect(current_path).to eq '/users'
-
-    click_link 'Joe Steiner'
-    expect(page).to have_content 'Joe Steiner'
-    expect(page).to have_content 'js@gmail.com'
-
-  end
-
   scenario "user sees validation message that first_name must be present" do
 
     visit root_path
+    click_link 'Sign In'
+    fill_in "Email", with: "js@gmail.com"
+    fill_in "Password", with: 'js'
+    click_button 'Sign In'
+
     click_link 'Users'
     expect(current_path).to eq '/users'
 
