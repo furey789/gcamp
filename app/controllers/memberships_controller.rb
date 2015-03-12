cdc790a8b7eb5a30d65bec4d8d09ebb80bcff122
Peter Furey <prfurey@gmail.com>
2015-03-12 14:44:01 -0600

    Format buttons on memberships index page, add functionality to buttons on index page, add glyphicon for deletion of members

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -12,7 +12,6 @@ class MembershipsController < ApplicationController
   end
 
 
-
   def create
     # @membership=@project.membership.new(params.require(:membership).permit(:user_id,:role,:project_id))
     @membership=Membership.new(params.require(:membership).permit(:user_id,:role).merge(project_id: params[:project_id]))
@@ -22,6 +21,23 @@ class MembershipsController < ApplicationController
     else
       render :index
     end
+  end
+
+  def update
+    @membership=Membership.find(params[:id])
+    if @membership.update(params.require(:membership).permit(:user_id,:role).merge(project_id: params[:project_id]))
+      flash[:notice]= "Member status was successfully updated"
+      redirect_to project_memberships_path
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @membership=Membership.find(params[:id])
+    flash[:notice]=@membership.user.full_name + ' was successfully removed'
+    @membership.destroy
+    redirect_to project_memberships_path
   end
 
 end
