8bcf398436119d9fd2d61c4754ef3637bdbb5184
Peter Furey <prfurey@gmail.com>
2015-03-17 15:21:39 -0600

    Add validation messages to membership update

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -2,23 +2,24 @@
 class MembershipsController < ApplicationController
 
   before_action do
-    @project=Project.find(params[:project_id])
     @membership_types=['member','owner']
   end
 
   def index
-    @memberships=@project.memberships
-    @membership=Membership.new
+    @project=Project.find(params[:project_id])
+    @memberships = @project.memberships
+    @membership = Membership.new
   end
 
 
   def create
-    # @membership=@project.membership.new(params.require(:membership).permit(:user_id,:role,:project_id))
+    @project=Project.find(params[:project_id])
     @membership=Membership.new(membership_params.merge(project_id: params[:project_id]))
     if @membership.save
       flash[:notice]= @membership.user.full_name + " was successfully added"
       redirect_to project_memberships_path
     else
+      @memberships = @project.memberships
       render :index
     end
   end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,6 +4,7 @@ class Membership < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
 
-  validates :user, presence: true
+  validates :user, presence: true,
+      uniqueness: {message: "has already been added to this project" }
 
 end
